a9ac75e1494fcb6fa014703ef6f1ff198f26e6b2
Omar <lerracxo@gmail.com>
2016-11-02 23:08:18 -0600

    Major changes, working on a new code improve version of code. This is the correct version of the project.

--- Prueba/src/com/ipn/esca/serviciosocial/mb/impl/SystemMB.java
+++ Prueba/src/com/ipn/esca/serviciosocial/mb/impl/SystemMB.java
@@ -3,20 +3,38 @@ package com.ipn.esca.serviciosocial.mb.impl;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 
-import com.ipn.esca.serviciosocial.common.Paginas;
+import com.ipn.esca.serviciosocial.common.Pages;
 
 @ManagedBean(name = "systemMB")
-@ViewScoped
+@SessionScoped
 public class SystemMB extends GenericMB implements Serializable {
 	private static final long serialVersionUID = 7930745241666648980L;
 	
+	private String actualPage; 
 	
-	public String navToMainPage(){
-		return Paginas.PRINCIPAL;
+	/** Function to control the flow and the active flag on left menu
+	 * @return
+	 */
+	public String getActualPage(){
+		return actualPage;
 	}
-
+	
+	public String getNavToMainPage(){
+		this.actualPage = Pages.PRINCIPAL;
+		return this.actualPage;
+	}
+	
+	public String getNavToInformation(){
+		this.actualPage = Pages.INFORMATION;
+		return this.actualPage;
+	}
+	
+	public String getNavToWelcome(){
+		this.actualPage = Pages.WELCOME;
+		return this.actualPage;
+	}
 	
 
 }
