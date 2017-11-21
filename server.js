d9fd6902b13fc473b3adb03fbfc7645277571c00
Oscar Santoyo <oscar.santoyo@wizeline.com>
2017-11-21 17:56:43 -0600

    fixes

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const bodyParser = require('body-parser')
 const routes = require('./api/routes/Routes')
 
 const port = process.env.PORT || 3000
-
+const angularContext = '/auth'
 global.project = {
-  projectDir: path.dirname(require.main.filename),
-  publicDir: path.dirname(require.main.filename) + '/public/',
-  uploadDir: path.dirname(require.main.filename) + '/files/',
+  projectDir: angularContext, //path.dirname(require.main.filename),
+  publicDir: angularContext + '/public/',//  path.dirname(require.main.filename) + '/public/',
+  uploadDir: angularContext + +'/files/', //path.dirname(require.main.filename) + '/files/',
   secret: 'secret'
 }
 
-app.set('superSecret', 'secretVar'); // secret variable
+app.set('superSecret', 'secretVar') // secret variable
 
 console.log('directory to public', project.projectDir)
 console.log('directory to public', project.publicDir)
 
 app.use(express.static(project.publicDir))
-app.use('/files',express.static(project.uploadDir))
+app.use('/files', express.static(project.uploadDir))
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
