e09fce7fa78ef0b7aa15df69c4dace86e36d8399
Oscar Santoyo <oscar.santoyo@wizeline.com>
2018-09-24 18:05:47 -0500

    Adding modifications to run on Google Cloud

--- api/database/DAO.js
+++ api/database/DAO.js
@@ -5,11 +5,11 @@ const message = require('../database/Message')
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
+  // host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-  port: 5432,
-  max: '10',
-  idleTimeoutMillis: 30000
+  // port: 5432,
+  // max: '10',
+  // idleTimeoutMillis: 30000
 }
 
 const pool = new Pool(config)
