8d25e4833ef4c33b0153b9b1f00ca0daa1cd4f26
Oscar Santoyo <oscar.santoyo@wizeline.com>
2018-09-26 13:20:23 -0500

    Adding connection options for heroku

--- api/database/DAO.js
+++ api/database/DAO.js
@@ -6,28 +6,22 @@ const config = {
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
   // host: process.env.DB_HOST,
-  database: process.env.SQL_DATABASE,
+  // database: process.env.SQL_DATABASE,
   // port: 5432,
   // max: '10',
   // idleTimeoutMillis: 30000
 }
 
-// console.log('Trying to connect with values', config)
-const pool = new Pool(config)
+console.log('Trying to connect with values', config)
+const pool = new Pool(processConnectionURL(process.env['DATABASE_URL']) || config)
 
-// console.log('pool', pool)
+console.log('pool', pool)
 
 testingConnection()
 
 function testingConnection(){
   connect((err, client, done) => {
-
-    if(err) {
-      console.error('The database couldnt get connected', err)
-    }
-    // console.log('err', err)
-    // console.log('client', client)
-    // console.log('done', done)
+    console.log('err', err)
   })
 }
 
@@ -77,5 +71,6 @@ function processConnectionURL (url) {
   config.host = util.split(':')[1].split('@')[1]
   config.port = util.split(':')[2].split('/')[0]
   config.database = util.split(':')[2].split('/')[1]
+  console.log('using processConnectionURL ', config)
   return config
 }
