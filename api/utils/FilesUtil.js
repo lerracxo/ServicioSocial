b9e6837109d529396354a79830911d2edbdff2f2
Oscar Santoyo <oscar.santoyo@wizeline.com>
2018-10-05 05:27:34 -0500

    fx

--- api/utils/FilesUtil.js
+++ api/utils/FilesUtil.js
@@ -7,16 +7,18 @@ const fs = require('fs')
 exports.uploadFile = function (req, fileNameWOext) {
   console.log('uploading a file')
   return new Promise((resolve, reject) => {
+    let fileName
+    let form
     try {
-      const form = new formidable.IncomingForm()
+      form = new formidable.IncomingForm()
+      console.log('control 1')
       form.multiples = false
+      console.log('control 1')
       form.uploadDir = project.uploadDir
-      let fileName
-      form.on('file', function (field, file) {
-        fileName = fileNameWOext + getExtension(file.name)
-        fs.renameSync(file.path, path.join(form.uploadDir, fileName))
-        console.log('Nombre archivo: ', fileName)
-      })
+      console.log('control 1')
+      
+      console.log('control 1')
+      form.on('file', onFile)
       form.on('error', function (err) {
         console.log('UPLOAD FILE: An error has occur: \n', err)
         reject(err)
@@ -29,8 +31,22 @@ exports.uploadFile = function (req, fileNameWOext) {
     catch(error) {
       reject(error)
     }
+
+    function onFile (field, file) {
+      try{
+        fileName = fileNameWOext + getExtension(file.name)
+        fs.renameSync(file.path, path.join(form.uploadDir, fileName))
+        console.log('Nombre archivo: ', fileName)
+      } catch (error) {
+        console.log('Error on OnFile ', error)
+      }
+    }
+
   })
 }
+
+
+
 
 exports.removeFile = function (file) {
   console.log('Archivo a eliminar', file)
