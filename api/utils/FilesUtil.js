3e875676c26f7f31103bd2fa78b80da43127f4e5
Oscar Santoyo <oscar.santoyo@wizeline.com>
2018-10-05 05:12:01 -0500

    fx

--- api/utils/FilesUtil.js
+++ api/utils/FilesUtil.js
@@ -7,23 +7,28 @@ const fs = require('fs')
 exports.uploadFile = function (req, fileNameWOext) {
   console.log('uploading a file')
   return new Promise((resolve, reject) => {
-    const form = new formidable.IncomingForm()
-    form.multiples = false
-    form.uploadDir = project.uploadDir
-    let fileName
-    form.on('file', function (field, file) {
-      fileName = fileNameWOext + getExtension(file.name)
-      fs.renameSync(file.path, path.join(form.uploadDir, fileName))
-      console.log('Nombre archivo: ', fileName)
-    })
-    form.on('error', function (err) {
-      console.log('UPLOAD FILE: An error has occur: \n', err)
-      reject(err)
-    })
-    form.on('end', function () {
-      resolve(fileName)
-    })
-    form.parse(req)
+    try {
+      const form = new formidable.IncomingForm()
+      form.multiples = false
+      form.uploadDir = project.uploadDir
+      let fileName
+      form.on('file', function (field, file) {
+        fileName = fileNameWOext + getExtension(file.name)
+        fs.renameSync(file.path, path.join(form.uploadDir, fileName))
+        console.log('Nombre archivo: ', fileName)
+      })
+      form.on('error', function (err) {
+        console.log('UPLOAD FILE: An error has occur: \n', err)
+        reject(err)
+      })
+      form.on('end', function () {
+        resolve(fileName)
+      })
+      form.parse(req)
+    }
+    catch(error) {
+      reject(error)
+    }
   })
 }
 
