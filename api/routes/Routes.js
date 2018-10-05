f015a6df5e2a8b6ff805258ee4f596d5208d15a5
Oscar Santoyo <oscar.santoyo@wizeline.com>
2018-10-05 02:12:13 -0500

    Removing index of controllers

--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -1,14 +1,29 @@
 'use strict'
-const controllers = require('../controllers/index.js')
-const contAuth = require('../controllers/Authentication')
+const personas = require('../controllers/Personas')
+const auth = require('../controllers/Authentication')
+const cursos = require('../controllers/Cursos')
+const calif = require('../controllers/Calificaciones')
+const materias = require('../controllers/Materias')
+const periodos = require('../controllers/Periodos')
+const upCalif = require('../controllers/DataUploadCalif')
+const upCurso = require('../controllers/DataUploadCurso')
+const utils = require('../controllers/Utils')
+
+
+const controllers = {
+    GET: [...personas.GET, ...auth.GET, ...cursos.GET, ...calif.GET, ...materias.GET, ...periodos.GET, ...upCalif.GET, ...upCurso.GET, ...utils.GET],
+    POST: [...personas.POST, ...auth.POST, ...cursos.POST, ...calif.POST, ...materias.POST, ...periodos.POST, ...upCalif.POST, ...upCurso.POST, ...utils.POST],
+    DELETE: [...personas.DELETE, ...auth.DELETE, ...cursos.DELETE, ...calif.DELETE, ...materias.DELETE, ...periodos.DELETE, ...upCalif.DELETE, ...upCurso.DELETE, ...utils.DELETE]
+}
+
 
 module.exports = function (app) {
 
   // Middleware
   app.use(function (req, res, next) {
-    contAuth.validateToken(req, res)
+    auth.validateToken(req, res)
       .then(next)
-      .catch((error) => contAuth.failedTokenValidation(res, error))
+      .catch((error) => auth.failedTokenValidation(res, error))
   })
 
   controllers.GET.forEach(route => app.route(route.endpoint).get(route.method))
